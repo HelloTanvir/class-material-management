7539078abadd408140fe944575dbd377f0035494
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-03-23 22:44:20 +0600

    upgraded to latest tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    purge: ['./src/pages/**/*.tsx', './src/components/**/*.tsx'],
-    darkMode: false, // or 'media' or 'class'
+    mode: 'jit',
+    content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+    darkMode: 'class',
     theme: {
         extend: {
             colors: {
